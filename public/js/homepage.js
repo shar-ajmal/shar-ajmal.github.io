1d100fdc1b1b9771a0eace2f3dea75244c4d9c49
Shaheryar Ajmal <sajmal@ucsd.edu>
2019-09-16 00:37:46 -0700

    Finished Homepage

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,35 +1,35 @@
 $(document).ready(function(){
     console.log("hello world");
-    // $('#debate-container').mouseover(function() {
-    //     $('#section-container').stop().fadeIn(300);
-    // });
+    $('#debate-container').mouseover(function() {
+        $('#section-container').stop().fadeIn(300);
+    });
 
-    // $('#debate-container').mouseout(function() {
-    //     $('#section-container').stop().fadeOut(300);
-    // });
+    $('#debate-container').mouseout(function() {
+        $('#section-container').stop().fadeOut(300);
+    });
 
-    // $('#doknosis-container').mouseover(function() {
-    //     $('#dok-container').stop().fadeIn(300);
-    // });
+    $('#doknosis-container').mouseover(function() {
+        $('#dok-container').stop().fadeIn(300);
+    });
 
-    // $('#doknosis-container').mouseout(function() {
-    //     $('#dok-container').stop().fadeOut(300);
-    // });
+    $('#doknosis-container').mouseout(function() {
+        $('#dok-container').stop().fadeOut(300);
+    });
 
-    // $('#souvenear-container').mouseout(function() {
-    //     $('#souv-container').stop().fadeOut(300);
-    // });
+    $('#souvenear-container').mouseout(function() {
+        $('#souv-container').stop().fadeOut(300);
+    });
 
-    // $('#souvenear-container').mouseover(function() {
-    //     $('#souv-container').stop().fadeIn(300);
-    // });
+    $('#souvenear-container').mouseover(function() {
+        $('#souv-container').stop().fadeIn(300);
+    });
 
-    // $('#pill-buddy-container').mouseout(function() {
-    //     $('#pill-container').stop().fadeOut(300);
-    // });
+    $('#pill-buddy-container').mouseout(function() {
+        $('#pill-container').stop().fadeOut(300);
+    });
 
-    // $('#pill-buddy-container').mouseover(function() {
-    //     $('#pill-container').stop().fadeIn(300);
-    // });
+    $('#pill-buddy-container').mouseover(function() {
+        $('#pill-container').stop().fadeIn(300);
+    });
     
 });
