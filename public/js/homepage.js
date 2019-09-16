c498757542a3f9c606cd056a5784fe5d1d91d1b2
Shaheryar Ajmal <sajmal@ucsd.edu>
2019-09-16 16:53:03 -0700

    Pushing homepage with links

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(window).on("load", function() {
     console.log("hello world");
     $('#debate-container').mouseover(function() {
         $('#section-container').stop().fadeIn(300);
@@ -31,5 +31,27 @@ $(document).ready(function(){
     $('#pill-buddy-container').mouseover(function() {
         $('#pill-container').stop().fadeIn(300);
     });
+
+    $('#doknosis-container').click(function(){
+        window.location.href='https://docs.google.com/presentation/d/17tukiTSiJ5I2dmy2eaM6M-9YNnpkhQyqZcA340-5MfM/edit?usp=sharing';
+     })
+
+     $('#souvenear-container').click(function() {
+        window.location.href='https://docs.google.com/presentation/d/1sxPruOTCDvZJnrpBXKFxS7AjcL3mhSyr4p9gq3Clqig/edit?usp=sharing';
+     })
+
+     $('#pill-buddy-container').click(function() {
+        window.location.href='https://docs.google.com/document/d/1F8mEG_RDhqBVqjhxstsadsHS8NrsxisY3eFdRrFUcqY/edit?usp=sharing'; 
+     })
+
+     $('#debate-container').click(function() {
+        window.location.href='http://ucsdguardian.org/2019/01/21/speech-debate-teams-achievements-signal-clubs-revitalization/'; 
+     })
+
+     $('#main-button').click(function() {
+        window.location.href='https://github.com/shar-ajmal/CardsAgainstHumanity';
+     })
+
+     $('.text-container').css('cursor', 'pointer');
     
 });
